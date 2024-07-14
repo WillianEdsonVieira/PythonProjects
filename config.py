4acce95b9a170e89854f66edac3f54b0df6333e9
Willian Vieira <130519777+WillianEdsonVieira@users.noreply.github.com>
2024-07-14 13:07:40 +0000

    modified:   DailyEmailReport      new file:   config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+
+SMTP_SERVER = 'smtp.seu_servidor.com'
+SMTP_PORT = 587  
+SMTP_USER = 'seu_email@exemplo.com'
+SMTP_PASSWORD = 'sua_senha'
+FROM_EMAIL = 'seu_email@exemplo.com'
+TO_EMAIL = 'destinatario@exemplo.com'
